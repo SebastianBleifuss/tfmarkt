9ffef53a171a724a0f753e283196a026e3fd89c7
Luca Patzelt <Luca.Patzelt@ruhr-uni-bochum.de>
2017-04-26 17:48:30 +0200

    Datastorage vorerst entfernt + xmlserialisierer erstellt

---
+++ xmlserializer/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlserializer
+{
+    public class Customer
+    {
+
+        public static List<String> getCustomerNames() {
+        List<String> CustomerNames = new List<string>();
+        foreach (String Name in Directory.GetFiles(xmlserializer.DATASTORAGEPATH + "\\customer")) {
+            CustomerNames.Add(Name.Replace(".xml", ""));
+        }
+        return CustomerNames;
+        }
+    }
+}
