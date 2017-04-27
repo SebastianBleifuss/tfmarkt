0c1eede40462c091ae4d02a736843c9ecba111da
Luca Patzelt <Luca.Patzelt@ruhr-uni-bochum.de>
2017-04-27 08:14:29 +0200

    Anpasungen am Interface und beheben des static const Fehlers

---
+++ tfMarktMain/TestKalkulation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xmlserializer.models;
+
+namespace tfMarktMain
+{
+    class TestKalkulation : xmlserializer.models.Calculation
+    {
+        //Lasst eure Kalkulationen bitte alle von xmlserializer.models.Calculation erben
+    }
+}
---
+++ xmlserializer/models/Calculation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlserializer.models
+{
+    public abstract class Calculation
+    {
+        public const Guid IDENTIFIER = Guid.NewGuid();
+    }
+}
