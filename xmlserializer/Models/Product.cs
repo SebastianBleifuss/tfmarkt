9787997a3b764d57478befb27779ea2621a6e148
Luca Patzelt <Luca.Patzelt@ruhr-uni-bochum.de>
2017-04-27 20:08:51 +0200

    Serialisierer für Customer + abstrakter Teil Calculations + funktionierende customer xml + Übernahme der Änderungen von Vivian

---
+++ xmlserializer/Models/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlserializer.Models
+{
+    public abstract class Product
+    {
+        protected int artikelnummer;
+        protected String artikelbezeichnung;
+        protected decimal preis;
+        protected Type ProductType;
+
+        public void setProductType(Type t) {
+            if (t.IsAssignableFrom(typeof(Calculation)))
+            {
+                this.ProductType = t;
+            }
+            else
+            {
+                throw new InvalidOperationException("Not iherit Calculations");
+            }
+        }
+
+        public Type getProductType() {
+            return ProductType;
+        }
+
+        public int getArtikelnummer()
+        {
+            return artikelnummer;
+        }
+
+        public String getArtikelbezeichnung()
+        {
+            return artikelbezeichnung;
+        }
+
+        public decimal getPreis()
+        {
+            return preis;
+        }
+    }
+}
--- xmlserializer/xmlserializer.cs
+++ xmlserializer/xmlserializer.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using xmlserializer.models;
+using System.Xml;
+using xmlserializer.Models;
+using xmlserializer.XmlDocuments;
 
 namespace xmlserializer
 {
@@ -14,32 +16,41 @@ namespace xmlserializer
         public static readonly string DATASTORAGEPATH = "..\\datastorage";
 
         public static void serialize(Customer Customer)
-        { 
-        
+        {
+            XmlDocument doc = new XmlDocument();
+
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement docElement = doc.DocumentElement;
+            doc.InsertBefore(xmlDeclaration, docElement);
+
+            XmlElement root = doc.CreateCustomerElement(Customer);
+            doc.AppendChild(root);
+
+            doc.Save(DATASTORAGEPATH + "\\customers\\" + Customer.Name.Replace(", ", "_") + ".xml");
         }
 
-        public static void serialize<T>(T Product) where T : IProducts
+        public static void serialize<T>(T Product) where T : Product
         {
 
         }
 
         public static Customer deserialize(String Customername)
         {
-            if (File.Exists(DATASTORAGEPATH + "\\customers\\" + Customername.Replace(", ", "_") + ".xml")) {
-                List<Calculation> ExampleCalcs = new List<Calculation>();
-                ExampleCalcs.Add(new FooBarCalculation());
-                return new Customer()
-                {
-                    Name = "example, customer",
-                    Calculations = ExampleCalcs
-                };
-            }else{
-            throw new FileNotFoundException();
+            String XmlPath = DATASTORAGEPATH + "\\customers\\" + Customername.Replace(", ", "_") + ".xml";
+            if (File.Exists(XmlPath))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(XmlPath);
+                return doc.GetCustomerFromElement(doc.DocumentElement);
             }
-           
+            else
+            {
+                throw new FileNotFoundException();
+            }
+
         }
 
-        public static List<IProducts> deserializeAllProducts()
+        public static List<Product> deserializeAllProducts()
         {
             //DATASTORAGEPATH + "\\products\\products.xml"
             return null;
