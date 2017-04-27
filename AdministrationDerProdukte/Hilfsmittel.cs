4cd3df86e4cb905cdf1096350f5dcee4fa9b43f7
SebastianBleifuss <simpson2913@hotmail.de>
2017-04-27 13:57:10 +0200

    Fachklassen für Tapete, Fliese und Hilfsmittel

---
+++ AdministrationDerProdukte/Hilfsmittel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xmlserializer.models;
+
+namespace AdministrationDerProdukte
+{
+    class Hilfsmittel: Product
+    {
+        private decimal ergiebigkeit;
+
+        public Hilfsmittel(String Artikelbezeichnung, decimal Ergiebigkeit, decimal Preis)
+        {
+            this.artikelbezeichnung = Artikelbezeichnung;
+            this.ergiebigkeit = Ergiebigkeit;
+            this.preis = Preis;
+        }
+    }
+}
