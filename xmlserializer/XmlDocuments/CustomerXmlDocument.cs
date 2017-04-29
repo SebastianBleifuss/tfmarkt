90856c1878450092c374f8159c52003c34ff8361
Luca Patzelt <Luca.Patzelt@ruhr-uni-bochum.de>
2017-04-30 00:17:24 +0200

    Documentation: - CustomerXmlDocument

--- xmlserializer/XmlDocuments/CustomerXmlDocument.cs
+++ xmlserializer/XmlDocuments/CustomerXmlDocument.cs
@@ -9,47 +9,66 @@ using xmlserializer.Models;
 
 namespace xmlserializer.XmlDocuments
 {
+    /// <summary>
+    /// Extention class for XmlDocument
+    /// </summary>
     public static class CustomerXmlDocument
     {
+
+        /// <summary>
+        /// Extention method for creating a Customer-XmlElement out of an customer instance
+        /// </summary>
+        /// <param name="doc">XmlDocument instance</param>
+        /// <param name="c">Customer instance for creating element</param>
+        /// <returns>Customer-XmlElement</returns>
         public static XmlElement CreateCustomerElement(this XmlDocument doc, Customer c)
         {
+            //Create root Element
             XmlElement root = doc.CreateElement(string.Empty, "Customer", string.Empty);
 
+            //Create NameElement
             XmlElement NameElement = doc.CreateElement(string.Empty, "Name", string.Empty);
             NameElement.AppendChild(
-                doc.CreateTextNode(c.Name)
+                doc.CreateTextNode(c.Name) //Create and add TextNode to NameElement child
             );
-            root.AppendChild(NameElement);
+            root.AppendChild(NameElement);//Add NameElement to root Element
 
+            //Create CalculationsElement
             XmlElement CalculationsElement = doc.CreateElement(string.Empty, "Calculations", string.Empty);
-            root.AppendChild(CalculationsElement);
+            root.AppendChild(CalculationsElement);//Add CalculationsElement to root Element
 
-            foreach (Calculation calc in c.Calculations.Values)
+            foreach (Calculation calc in c.Calculations.Values)//Create and add all Calculations as XmlElements
             {
                 CalculationsElement.AppendChild(
-                        doc.CreateCalculationElement(calc)
+                        doc.CreateCalculationElement(calc)//Create calculation-XmlElement from calculation instance
                 );
             }
 
 
-            return root;
+            return root;//Return root Element
         }
 
+        /// <summary>
+        /// Extention method for creating a customer Element out of an customer instance
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="CustomerElement">Customer Element from where the calculations are loaded</param>
+        /// <returns></returns>
         public static Customer GetCustomerFromElement(this XmlDocument doc, XmlElement CustomerElement)
         {
             Customer LoadingCustomer = new Customer();
-            LoadingCustomer.Name = CustomerElement.SelectSingleNode("Name").InnerText;
+            LoadingCustomer.Name = CustomerElement.SelectSingleNode("Name").InnerText;//Set InnerText of "Name"-Node into Customer.Name-Property
             LoadingCustomer.Calculations = new Dictionary<Guid, Calculation>();
             foreach (XmlNode calcNode in CustomerElement.SelectSingleNode("Calculations").ChildNodes)
             {
-                if (calcNode.Name.Equals("Calculation"))
+                if (calcNode.Name.Equals("Calculation"))//Check if Node is Calculation
                 {
-                    Calculation calc = doc.GetCalculationFromNode(calcNode);
-                    LoadingCustomer.Calculations.Add(calc.Identifier, calc);
+                    Calculation calc = doc.GetCalculationFromNode(calcNode);//Get calculation from passed element
+                    LoadingCustomer.Calculations.Add(calc.Identifier, calc);//Add Calculation 
                 }
             }
 
-            return LoadingCustomer;
+            return LoadingCustomer;//Return Customer
         }
     }
 }
